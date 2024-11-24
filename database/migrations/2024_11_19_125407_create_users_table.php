afc2c65b57a7c5df6791b9e8f6ff7c650596cafd
Andrew <andrettery@gmail.com>
2024-11-24 14:15:56 +0200

    first commit

---
+++ database/migrations/2024_11_19_125407_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cart_id');
+            $table->string('email',length: 100)->unique();
+            $table->string('password',length: 255);
+            $table->foreign('role_id')->references('id')->on('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
